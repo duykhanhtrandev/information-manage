69f07770cbf56c3590b36f22a42f9379a72e68cc
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2022-10-22 01:11:44 +0700

    use npm

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('run request ...');
   res.setHeader('Content-Type', 'text/html');
-  res.write('<h3>Hello world! </h3>');
-  res.write('<h2>from ERIC & HOI DAN IT </h2>');
+  // res.write('<h3>Hello world! </h3>');
+  // res.write('<h2>from ERIC & HOI DAN IT </h2>');
   res.end();
 });
 
